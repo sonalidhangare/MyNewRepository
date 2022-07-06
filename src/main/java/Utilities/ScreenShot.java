88bb7f8d820b7615c518e075f24259eb15063f98
Dr.Robot <Dr.Robot@DESKTOP-F4S4O5V>
2022-07-06 13:59:21 +0530

    My Amazon Repository

---
+++ src/main/java/Utilities/ScreenShot.java
@@ -0,0 +1,26 @@
+package Utilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.AfterTest;
+
+public class ScreenShot {
+	WebDriver driver;
+	@AfterTest
+	public void clickScreenShot() throws IOException
+	{
+		File source=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		File dest=new File("E:\\Velocity-JavaAutomation2022\\amazonlogin\\Screenshots\\Amz.jpg");
+		
+		FileHandler.copy(source, dest);
+		
+		
+	}
+
+}
